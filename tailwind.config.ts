01e91e19b5fb64284faffb1e673df93b22f754d5
Pablo Gambetta <gambeita@gmail.com>
2026-02-22 09:27:51 -0300

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      height: {
+        // @ts-ignore
+        screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
